abfe927ed4c5447ccde1880311abeed66d98bf3c
CreepGit <julius.vaisto@gmail.com>
2023-02-17 14:06:47 +0200

    Cookie message

--- vue/client/src/App.vue
+++ vue/client/src/App.vue
@@ -25,6 +25,11 @@ onMounted(()=>{
     document.body.style.transition = ""
   }, 0)
 })
+
+const cookies = ref(false)
+setTimeout(()=>{
+  cookies.value = true
+}, 1500)
 </script>
 
 <template>
@@ -48,6 +53,16 @@ onMounted(()=>{
     </div>
   </header>
   <RouterView />
+  <Transition name="cookie-transition">
+    <dialog class="cookie" v-if="cookies" open @click="cookies=false">
+      <div>
+        <h2>This webpage uses cookies! 🍪</h2>
+        <p>But don't worry, we won't sell your data to any big corp, or anyone else.</p>
+        <p>Click here to discard this message.</p>
+      </div>
+      <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpurepng.com%2Fpublic%2Fuploads%2Flarge%2Fpurepng.com-cookiefood-bakery-sweet-cookie-biscuit-941524621515qlctx.png&f=1&nofb=1&ipt=4775ddaed5e03227d733a58df57d073c8e73a01f5af512c90c2356e31f8fd2b7&ipo=images" alt="">
+    </dialog>
+  </Transition>
 </template>
 
 <style scoped>
@@ -88,6 +103,68 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
+}
+
+dialog.cookie {
+  position: fixed;
+  bottom: 50px;
+  left: 50%;
+  translate: -50%;
+  color: inherit;
+  border-radius: 0 20px 20px 0;
+  border: none;
+  padding: 3px;
+  padding-left: 0;
+  background: linear-gradient(110deg, transparent, rgb(0, 189, 126));
+}
+
+dialog.cookie:hover {
+  background: linear-gradient(110deg, transparent, red);
+}
+
+dialog.cookie div {
+  background: linear-gradient(90deg, var(--color-background), var(--color-background-soft));
+  padding: 20px 40px 20px 40px;
+  border-radius: 0 20px 20px 0;
+}
+
+dialog.cookie img {
+  position: absolute;
+  bottom: -50px;
+  right: calc(100% - 20px);
+  height: 200px;
+  animation: spin 4s infinite;
+  pointer-events: none;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate3d(0, 1, 0, 0deg);
+    filter: drop-shadow(0px 0px 3px wheat);
+  }
+  70% {
+    transform: rotate3d(0, 1, 0, 360deg);
+    filter: drop-shadow(0px 0px 20px goldenrod);
+  }
+  100% {
+    transform: rotate3d(0, 1, 0, 360deg);
+    filter: drop-shadow(0px 0px 3px wheat);
+  }
+}
+
+.cookie-transition-enter-active,
+.cookie-transition-leave-active {
+  transition: all 2s ease;
+  opacity: 100%;
+}
+
+.cookie-transition-enter-from,
+.cookie-transition-leave-to {
+  transform: translateX(200px);
+  opacity: 0%;
+}
+.cookie-transition-leave-to {
+  transform: translateY(70px);
 }
 
 @media (min-width: 1024px) {

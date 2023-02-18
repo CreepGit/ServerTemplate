647cccbaeee4709b2e1d73dbac3d39888c954e06
CreepGit <julius.vaisto@gmail.com>
2023-02-18 08:36:47 +0200

    Cookie moved to its own component

--- vue/client/src/App.vue
+++ vue/client/src/App.vue
@@ -4,6 +4,7 @@ import { ref, watch, onMounted, onBeforeMount, type Ref } from 'vue';
 import { useStateStore } from '@/stores/state'
 import HelloWorld from './components/HelloWorld.vue'
 import Switch from './components/ToggleSwitch.vue'
+import Cookie from './components/CookieDialog.vue'
 
 const store = useStateStore()
 const themeSwitcherValue: Ref<boolean> = ref(store.currentTheme == "dark")
@@ -25,11 +26,6 @@ onMounted(()=>{
     document.body.style.transition = ""
   }, 0)
 })
-
-const cookies = ref(false)
-setTimeout(()=>{
-  cookies.value = true
-}, 1500)
 </script>
 
 <template>
@@ -46,29 +42,24 @@ setTimeout(()=>{
         <a href="/admin/">Admin</a>
       </nav>
       <div style="display: flex; justify-content: space-between; width: 100%;">
-        <h3>Theme Selector: </h3>
+        <h3 class="header">Theme Selector: </h3>
         <h3>🌞 <Switch class="themeSwitch" v-model="themeSwitcherValue" /> 🌙</h3>
       </div>
       <span>Notice how it does not flash the screen on refresh on either of the two themes.</span>
     </div>
   </header>
   <RouterView />
-  <Transition name="cookie-transition">
-    <dialog class="cookie" v-if="cookies" open @click="cookies=false">
-      <div>
-        <h2>This webpage uses cookies! 🍪</h2>
-        <p>But don't worry, we won't sell your data to any big corp, or anyone else.</p>
-        <p>Click here to discard this message.</p>
-      </div>
-      <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpurepng.com%2Fpublic%2Fuploads%2Flarge%2Fpurepng.com-cookiefood-bakery-sweet-cookie-biscuit-941524621515qlctx.png&f=1&nofb=1&ipt=4775ddaed5e03227d733a58df57d073c8e73a01f5af512c90c2356e31f8fd2b7&ipo=images" alt="">
-    </dialog>
-  </Transition>
+  <Cookie />
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
+}
+
+.header {
+  color: var(--color-heading);
 }
 
 :deep(.themeSwitch.active .ball) {
@@ -103,68 +94,6 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
-}
-
-dialog.cookie {
-  position: fixed;
-  bottom: 50px;
-  left: 50%;
-  translate: -50%;
-  color: inherit;
-  border-radius: 0 20px 20px 0;
-  border: none;
-  padding: 3px;
-  padding-left: 0;
-  background: linear-gradient(110deg, transparent, rgb(0, 189, 126));
-}
-
-dialog.cookie:hover {
-  background: linear-gradient(110deg, transparent, red);
-}
-
-dialog.cookie div {
-  background: linear-gradient(90deg, var(--color-background), var(--color-background-soft));
-  padding: 20px 40px 20px 40px;
-  border-radius: 0 20px 20px 0;
-}
-
-dialog.cookie img {
-  position: absolute;
-  bottom: -50px;
-  right: calc(100% - 20px);
-  height: 200px;
-  animation: spin 4s infinite;
-  pointer-events: none;
-}
-
-@keyframes spin {
-  0% {
-    transform: rotate3d(0, 1, 0, 0deg);
-    filter: drop-shadow(0px 0px 3px wheat);
-  }
-  70% {
-    transform: rotate3d(0, 1, 0, 360deg);
-    filter: drop-shadow(0px 0px 20px goldenrod);
-  }
-  100% {
-    transform: rotate3d(0, 1, 0, 360deg);
-    filter: drop-shadow(0px 0px 3px wheat);
-  }
-}
-
-.cookie-transition-enter-active,
-.cookie-transition-leave-active {
-  transition: all 2s ease;
-  opacity: 100%;
-}
-
-.cookie-transition-enter-from,
-.cookie-transition-leave-to {
-  transform: translateX(200px);
-  opacity: 0%;
-}
-.cookie-transition-leave-to {
-  transform: translateY(70px);
 }
 
 @media (min-width: 1024px) {
---
+++ vue/client/src/components/CookieDialog.vue
@@ -0,0 +1,96 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+const cookies = ref(false)
+setTimeout(()=>{
+  cookies.value = true
+}, 1500)
+</script>
+
+<template>
+  <Transition name="cookie-transition">
+    <dialog class="cookie" v-if="cookies" open @click="cookies=false">
+      <div>
+        <h2>This webpage uses cookies! 🍪</h2>
+        <p>But don't worry, we won't sell your personal data to a big corporation, or anyone else for that matter.
+        <span class="highlightOnHover">Click to discard</span> this message or <span @click.stop class="learnMore">click here to learn more.</span></p>
+      </div>
+      <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpurepng.com%2Fpublic%2Fuploads%2Flarge%2Fpurepng.com-cookiefood-bakery-sweet-cookie-biscuit-941524621515qlctx.png&f=1&nofb=1&ipt=4775ddaed5e03227d733a58df57d073c8e73a01f5af512c90c2356e31f8fd2b7&ipo=images" alt="">
+    </dialog>
+  </Transition>
+</template>
+
+<style scoped>
+dialog.cookie {
+  position: fixed;
+  bottom: 50px;
+  left: 50%;
+  translate: -50%;
+  color: inherit;
+  border-radius: 0 20px 20px 0;
+  border: none;
+  padding: 3px;
+  padding-left: 0;
+  background: linear-gradient(110deg, transparent, rgb(0, 189, 126));
+}
+
+dialog.cookie:hover {
+  background: linear-gradient(110deg, transparent, red);
+  filter: drop-shadow(0 0 15px red);
+  cursor: pointer;
+}
+
+dialog.cookie:hover .highlightOnHover {
+  color: red;
+  text-decoration: underline;
+}
+
+.learnMore:hover {
+  color: hsla(160, 100%, 37%, 1);
+  text-decoration: underline;
+}
+
+dialog.cookie div {
+  background: linear-gradient(90deg, var(--color-background), var(--color-background-soft));
+  padding: 20px 40px 20px 40px;
+  border-radius: 0 20px 20px 0;
+}
+
+dialog.cookie img {
+  position: absolute;
+  bottom: -50px;
+  right: calc(100% - 20px);
+  height: 200px;
+  animation: spin 4s infinite;
+  pointer-events: none;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate3d(0, 10, -1, 0deg);
+    filter: drop-shadow(0px 0px 3px wheat);
+  }
+  70% {
+    transform: rotate3d(0, 10, -1, 360deg);
+    filter: drop-shadow(0px 0px 20px goldenrod);
+  }
+  100% {
+    transform: rotate3d(0, 10, -1, 360deg);
+    filter: drop-shadow(0px 0px 3px wheat);
+  }
+}
+
+.cookie-transition-enter-active,
+.cookie-transition-leave-active {
+  transition: all 2s ease;
+  opacity: 100%;
+}
+
+.cookie-transition-enter-from,
+.cookie-transition-leave-to {
+  transform: translateX(200px);
+  opacity: 0%;
+}
+.cookie-transition-leave-to {
+  transform: translateY(70px);
+}
+</style>

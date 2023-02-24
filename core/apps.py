8dec748d6271a7eed49b3c9071b59eed41b214af
CreepGit <julius.vaisto@gmail.com>
2023-02-24 11:50:28 +0200

    Automatically add specifiers

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
+from allauth.account.signals import user_signed_up
+from core.models import User
+import random
+import string
+
+
+def add_missing_user_data(request, user: User, **kwargs):
+    characters = string.ascii_uppercase + string.digits
+    if not user.specifier:
+        user.specifier = "".join(random.choices(characters, k=4))
+        user.save()
 
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
+
+    def ready(self) -> None:
+        user_signed_up.connect(add_missing_user_data)
+        return super().ready()

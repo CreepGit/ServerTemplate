58b0a57628667c49aeb0c50baca60e9014bf1d90
CreepGit <julius.vaisto@gmail.com>
2023-02-26 07:53:54 +0200

    About view to "About you" view

--- vue/client/src/views/AboutView.vue
+++ vue/client/src/views/AboutView.vue
@@ -1,6 +1,17 @@
+<script setup lang="ts">
+import { useStateStore } from "@/stores/state";
+const state = useStateStore()
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About you</h1>
+    <table>
+      <tr v-for="[key, value] in Object.entries(state.user as any)">
+        <td>{{ key }}</td>
+        <td>{{ value }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -10,6 +21,8 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    flex-direction: column;
+    justify-content: center;
   }
 }
 </style>

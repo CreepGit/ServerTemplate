a64b86ed63f1847191c1490635074d0d40d3a270
CreepGit <julius.vaisto@gmail.com>
2023-02-26 10:10:45 +0200

    REST permissions

--- server/rest.py
+++ server/rest.py
@@ -1,4 +1,4 @@
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, serializers, viewsets, mixins
 from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -7,20 +7,62 @@ from django.contrib.auth import get_user_model
 from django.urls import path, include, re_path
 from rest_framework.reverse import reverse
 from django.http import JsonResponse
+from rest_framework import permissions
 
 
 User = get_user_model()
 
 
+class IsMeOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj == request.user
+
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ["url", "slug", "username", "email", "is_staff", "is_superuser", "is_active", "groups", "date_joined", "last_login"]
+        fields = [
+            "url",
+            "slug",
+            "username",
+            "email",
+            "is_staff",
+            "is_superuser",
+            "is_active",
+            "groups",
+            "date_joined",
+            "last_login",
+        ]
+        read_only_fields = [
+            "url",
+            "email",
+            "is_staff",
+            "is_superuser",
+            "is_active",
+            "groups",
+            "date_joined",
+            "last_login",
+        ]
 
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
+
+class LookAndUpdateViewset(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    """Allows looking (get) and updating (patch) but nothing else"""
+
+    pass
+
+
+class UserViewSet(LookAndUpdateViewset):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsMeOrReadOnly]
 
 
 class CurrentUserView(APIView):
@@ -43,10 +85,12 @@ class RootView(APIView):
 
 
 def rest404view(request: WSGIRequest):
-    return JsonResponse({
-        "error": "No such page",
-    }, status = 404)
-
+    return JsonResponse(
+        {
+            "error": "No such page",
+        },
+        status=404,
+    )
 
 
 router = routers.SimpleRouter(trailing_slash=True)
